d43b38691f2748aba8902aaf3d171c916a4c3de3
rsnf <roberto.sarquis@gmail.com>
2023-08-06 10:29:59 -0300

    Custom annotation with parameters.

--- spring-mvc-validation/src/main/java/com/sqs/springdemo/mvc/Customer.java
+++ spring-mvc-validation/src/main/java/com/sqs/springdemo/mvc/Customer.java
@@ -24,7 +24,7 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
-    @CourseCode
+    @CourseCode(value = "SQS", message = "must start with SQS")
     private String courseCode;
 
     public String getFirstName() {
